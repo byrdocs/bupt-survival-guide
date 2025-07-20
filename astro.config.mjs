5b5ed5fbd5f6e15113f72b1731dcdb9053262e2f
maredevi <maredevi@outlook.com>
2025-07-20 23:50:38 +0800

    fix: wow

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,15 @@ export default defineConfig({
   integrations: [
     starlight({
       title: "BUPT 生存手册",
+      locales: {
+        root: {
+          label: "简体中文",
+          lang: "zh-CN",
+        },
+      },
+      editLink: {
+        baseUrl: "https://github.com/byrdocs/SurviveBUPTManual/edit/main/",
+      },
       social: [
         {
           icon: "github",
@@ -24,16 +33,15 @@ export default defineConfig({
             { label: "住宿条件", slug: "shahe-campus/dormitory" },
             { label: "学业相关", slug: "shahe-campus/academic" },
             { label: "生活相关", slug: "shahe-campus/life" },
-            { label: "内网资源", slug: "shahe-campus/intranet" },
-            { label: "军训相关", slug: "shahe-campus/military-training" },
+            { label: "网络资源", slug: "shahe-campus/network" },
             { label: "学生组织", slug: "shahe-campus/organization" },
             { label: "其他事项", slug: "shahe-campus/misc" },
           ],
         },
+        { label: "搬家攻略", slug: "moving-guide" },
         {
           label: "本部校区指南",
           items: [
-            { label: "搬家攻略", slug: "main-campus/registration" },
             { label: "宿舍条件", slug: "main-campus/dormitory" },
             { label: "生活相关", slug: "main-campus/life" },
             { label: "学习环境", slug: "main-campus/academic" },
@@ -41,10 +49,6 @@ export default defineConfig({
             { label: "交通出行", slug: "main-campus/transportation" },
             { label: "生活服务", slug: "main-campus/services" },
           ],
-        },
-        {
-          label: "参考资料",
-          autogenerate: { directory: "reference" },
         },
         {
           label: "贡献指南",
